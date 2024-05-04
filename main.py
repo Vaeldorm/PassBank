5e4065aa2d7025c20d81ab851e281f94091c5fa3
Vaeldorm <Dylan.Skinkis@gmail.com>
2024-05-04 13:46:02 -0500

    Program is WIP, do NOT to store sensitive data
    New features:
        User interface
        Bank class
        Connection to PostgreSQL database

--- main.py
+++ main.py
@@ -2,13 +2,78 @@
 Goal: Learn to interface with postgres database, and (additionally) encrypt/decrypt data. Potential expansion: Create an API
 """
 
+import os
 import pandas as pd
+from pangres import upsert
+import psycopg2
 import secrets
 import string
+from sqlalchemy import text, create_engine
 import sys
 
+from dotenv import load_dotenv
+load_dotenv(override=True)
 
-def in_venv():
+class Bank:
+    def __init__(self, db_credentials):
+        self.db_credentials = db_credentials
+        self.connection_url = f"postgresql+psycopg2://{self.db_credentials['db_username']}:{self.db_credentials['db_password']}@{self.db_credentials['db_host']}:{self.db_credentials['db_port']}/{self.db_credentials['db_name']}"
+        # TODO DELETE?  self.action = None
+        self.connection = None
+        self.entry = None 
+
+    def connect(self):
+        self.db_engine = create_engine(self.connection_url, pool_recycle=3600)
+        self.connection = self.db_engine.connect()
+        return self.connection
+
+    def find_account(self, account='') -> list:
+        # Using parameterized query to avoid SQL injection
+        query = '''SELECT account, username, password
+                FROM gucci.accounts
+                WHERE account = %s;'''
+        df = pd.read_sql(query, self.connection, params=(account,))
+        self.entry = df
+
+    def gather_info(self):
+        account = get_account()
+        username = get_username()
+        password = get_password()
+
+        data = {'account': [account], 'username': [username], 'password': [password]}
+        df = pd.DataFrame(data)
+        df.set_index('account', inplace=True)
+        return df
+
+    def set_entry(self):
+        self.entry = self.gather_info()
+
+    def upsert(self, schema='gucci', table_name='accounts', if_row_exists='update'):
+        self.entry = self.gather_info()
+        if self.entry is not None:
+            upsert(con=self.connection, df=self.entry, schema=schema, table_name=table_name, create_table=True, create_schema=True, if_row_exists=if_row_exists)
+            self.connection.commit()  # Commit the transaction
+        else:
+            print("No entry to upsert.")
+   
+    def get_mode(self):
+        '''
+        Let user select if they wish to read or write information.
+        '''
+        modes = ['Add', 'View']
+        print('What would you like to do?')
+        for num, option in enumerate(modes):
+            print(f'{num + 1}. {option}')
+        response = input('')
+        match response:
+            case '1':
+                self.upsert()
+            case '2':
+                #TODO print list of accounts
+                account = input('Which account do you wish to view? ')
+                self.find_account(account)
+
+def in_venv(): # Set up test file to move this function
     '''
     Compare with the purpose of ensuring that virtual environment is active.
     Works by confirming base environment is not the current environment
@@ -16,7 +81,7 @@ def in_venv():
     '''
     return sys.prefix != sys.base_prefix
 
-def dev_interface():
+def dev_interface(): # Set up test file to move this function
     '''
     Interface with functions designed for testing the environment
     '''
@@ -40,7 +105,6 @@ def dev_interface():
         case _:
             response = dev_interface()
     return response
-
 
 def get_account() -> str:
     '''
@@ -68,44 +132,40 @@ def generate_password() -> str:
 
 def get_password():
     password = ''
-    response = input('Would you like to generate a password? y/n')
+    response = input('Would you like to generate a password? y/n: ')
     match response:
         case 'y':
             password = generate_password()
-        case 'n':
-            password = input()
+        case 'n': #TODO Add pw match checking
+            password = input('Password: ')
         case _:
             password = get_password()
     return password
 
-def add(account):
-    entry_data = []
-    entry_data.append(account)
-    entry_data.append(get_username)
-    entry_data.append(get_password)
-    df = pd.DataFrame(entry_data)
-    return entry_data
+# test = dev_interface()
+# print(test )
 
-def view():
-    '''
-    Retrieve username and password from database for a selected account.
-    '''
-    pass
+db_credentials = {
+    'db_username': os.environ.get('db_username'),
+    'db_password': os.environ.get('db_password'),
+    'db_host': os.environ.get('db_host'),
+    'db_port': os.environ.get('db_port'),
+    'db_name': os.environ.get('db_name')
+}
 
-def get_mode():
-    '''
-    Let user select if they wish to read or write information.
-    '''
-    modes = ['Add', 'View']
-    response = input('What would you like to do?')
-    for num, option in enumerate(modes):
-        print(f'{num}. {option}')
-    match response:
-        case '1':
-            info = add()
-        case '2':
-            info = view()
-    return info
+passbank = Bank(db_credentials)
+db = passbank.db_credentials['db_name']
 
-test = dev_interface()
-print(test )
+passbank.connect()
+print(f'Connecting to {db}')
+if passbank.connection != None:
+    print(f'{db} connection successful')
+else: print(f'Error connecting to {db}')
+
+passbank.get_mode()
+
+print('\n')
+print(passbank.entry)
+
+passbank.connection.close()
+print(f'Disconnected from {db}')
